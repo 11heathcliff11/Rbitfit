855b841ef644bca286023977ba38c5886a499a61
chrlie <cdelassence@cdelassence-macbookpro.roam.corp.google.com>
2016-02-04 07:15:58 +0100

    new key

--- api-access-test.R
+++ api-access-test.R
@@ -1,4 +1,3 @@
-
 # install_github("hadley/httr")
 library(httr)
 
@@ -10,7 +9,7 @@ fitbit_endpoint <- oauth_endpoint(
 myapp <- oauth_app(
     appname = "cdlr",
     key = "227FWR", 
-    secret = "97285ac3fc9fdf1ee3e7a30a15693e8b")
+    secret = "3089e3d1ac5dde1aa00b54a0c8661f42")
 
 # 2. Get OAuth token
 scope <- c("sleep","activity")  # See dev.fitbit.com/docs/oauth2/#scope
@@ -18,6 +17,9 @@ fitbit_token <- oauth2.0_token(fitbit_endpoint, myapp,
                                scope = scope, use_basic_auth = TRUE)
 
 # 3. Make API requests
-resp <- GET(url = "https://api.fitbit.com/1/user/-/activities/date/2016-01-30.json", 
+
+resp <- GET(url = "https://api.fitbit.com/1/user/2RY8WC/activities/date/2016-02-01.json", 
             config(token = fitbit_token))
-resp
+
+str(resp)
+resp$content
