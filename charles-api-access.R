d6a80fe40e3cbf49c0cd19ee80cf1e489a6f64fb
chrlie <lassence@gmail.com>
2016-02-07 16:45:01 +0100

    created functions

--- charles-api-access.R
+++ charles-api-access.R
@@ -4,46 +4,62 @@
 
 library('httr')
 
-# 1. Set up credentials
-fitbit_endpoint <- oauth_endpoint(
-    request = "https://api.fitbit.com/oauth2/token",
-    authorize = "https://www.fitbit.com/oauth2/authorize",
-    access = "https://api.fitbit.com/oauth2/token")
-myapp <- oauth_app(
-    appname = "cdlr",
-    key = "227FWR", 
-    secret = "3089e3d1ac5dde1aa00b54a0c8661f42")
+# Setup API connection
 
-# 2. Get OAuth token
-# Scope: see dev.fitbit.com/docs/oauth2/#scope
-scope <- c("activity", "heartrate", "location", "nutrition", "profile", "settings", "sleep", "social", "weight")
-fitbit_token <- oauth2.0_token(fitbit_endpoint, 
-                               myapp,
-                               scope = scope, 
-                               use_basic_auth = TRUE)
+apiConnection <- function(appname = "cdlr", key = "227FWR", secret = "3089e3d1ac5dde1aa00b54a0c8661f42") {
+    
+    # Set up credentials
+    fitbit_endpoint <- oauth_endpoint(
+        request = "https://api.fitbit.com/oauth2/token",
+        authorize = "https://www.fitbit.com/oauth2/authorize",
+        access = "https://api.fitbit.com/oauth2/token")
+    
+    myapp <- oauth_app(appname, key, secret)
+    
+    # Get OAuth token
+    # Scope: see dev.fitbit.com/docs/oauth2/#scope
+    scope <- c("activity", "heartrate", "location", "nutrition", "profile", "settings", "sleep", "social", "weight")
+    fitbit_token <<- oauth2.0_token(fitbit_endpoint, 
+                                    myapp,
+                                    scope = scope, 
+                                    use_basic_auth = TRUE)
 
-# 3. Make API requests
-date <- "2016-02-03"
-get_url <- paste("https://api.fitbit.com/1/user/-/activities/date/", 
-                date, 
-                ".json", 
-                sep = "")
-response <- GET(url = get_url, config(token = fitbit_token))
+}
 
-# 4. Write content to file
-resp_content <- content(response, as = "text")
-json_file <- "apicontent.json"
-writeBin(resp_content, json_file)
+
+
+# Makes API request and writes JSON files (1 per day of data)
+getApiContent <- function(date = "2016-02-03", json_file = "daily-summary.json") {
+    
+    # Build URL with date for request (daily activity summary)
+    # Cf. https://dev.fitbit.com/docs/activity/#get-daily-activity-summary
+    get_url <- paste("https://api.fitbit.com/1/user/-/activities/date/", 
+                     date, 
+                     ".json", 
+                     sep = "")
+
+    # Send the request
+    response <- GET(url = get_url, config(token = fitbit_token))
+
+    # Writes the response content into a JSON file
+    resp_content <- content(response, as = "text")
+    writeBin(resp_content, json_file)
+    
+}
+
+# Function calls
+apiConnection()
+getApiContent()
 
 ###
 ### Convert JSON to Data Frame
-### Begin Charles
 ### 
 
 library('rjson')
 library('jsonlite')
 # library('data.table')
 
+json_file <- "daily-summary.json"
 json_raw <- rjson::fromJSON(file = json_file)
 
 # Conversion to simple vector
@@ -54,5 +70,4 @@ json_vector <- unlist(json_raw)
 
 
 ###
-### End Charles
 ###
