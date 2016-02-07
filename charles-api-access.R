7c9e091f16e4a7c210ad6a59758f315bf70977a1
chrlie <cdelassence@cdelassence-macbookpro.roam.corp.google.com>
2016-02-07 14:38:37 +0100

    starting code for JSON conversion

--- charles-api-access.R
+++ charles-api-access.R
@@ -1,5 +1,8 @@
+###
+### API Access
+###
 
-library(httr)
+library('httr')
 
 # 1. Set up credentials
 fitbit_endpoint <- oauth_endpoint(
@@ -14,17 +17,32 @@ myapp <- oauth_app(
 # 2. Get OAuth token
 # Scope: see dev.fitbit.com/docs/oauth2/#scope
 scope <- c("activity", "heartrate", "location", "nutrition", "profile", "settings", "sleep", "social", "weight")
-fitbit_token <- oauth2.0_token(fitbit_endpoint, myapp,
-                               scope = scope, use_basic_auth = TRUE)
+fitbit_token <- oauth2.0_token(fitbit_endpoint, 
+                               myapp,
+                               scope = scope, 
+                               use_basic_auth = TRUE)
 
 # 3. Make API requests
 date <- "2016-02-03"
-geturl <- paste("https://api.fitbit.com/1/user/-/activities/date/", 
+get_url <- paste("https://api.fitbit.com/1/user/-/activities/date/", 
                 date, 
                 ".json", 
                 sep = "")
-resp <- GET(url = geturl, config(token = fitbit_token))
+response <- GET(url = get_url, config(token = fitbit_token))
 
 # 4. Write content to file
-respContent <- content(resp, as = "text")
-writeBin(respContent, "apicontent.json")
+resp_content <- content(response, as = "text")
+writeBin(resp_content, "apicontent.json")
+
+###
+### Convert JSON to Data Frame
+### Begin Charles
+### 
+
+library(c('rjson', 'jsonlite'))
+json_file <- "apicontent.json"
+json_df <- rjson::fromJSON(file = json_file)
+
+###
+### End Charles
+###
