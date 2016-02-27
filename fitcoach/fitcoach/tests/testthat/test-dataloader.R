0d7fa2acf06b9dd99f6b026856adc6766ad65e16
chrlie <lassence@gmail.com>
2016-02-27 14:12:53 +0100

    cleaner tests for DataLoader

--- fitcoach/fitcoach/tests/testthat/test-dataloader.R
+++ fitcoach/fitcoach/tests/testthat/test-dataloader.R
@@ -1,58 +1,33 @@
 library('testthat')
 library('R6')
 
-### Bulk requests using our DataLoader object
-
-expect_output(
-        BulkRequest <- DataLoader$new(), 
-        "Object DataLoader initialized"
-    )
-
-BulkRequest$connect(appname = "cdlr",
-                    key = "227FWR",
-                    secret = "3089e3d1ac5dde1aa00b54a0c8661f42"
-                    )
-
-expect_equal(BulkRequest$api_token$app$key, "227FWR")
-
-BulkRequest$requestAndWrite(
-    type = 'day', 
-    activities = c('steps', 'calories', 'minutesLightlyActive'), 
-    start_date = "2016-01-20", 
-    end_date = "2016-02-05", 
-    path = "./inst/extdata/tests/")
-
-
-BulkRequest$requestAndWrite(
-    type = 'intraday', 
-    activities = c('steps', 'calories', 'distance'), 
-    start_date = "2016-02-01", 
-    path = "./inst/extdata/tests/")
-
-# expect_equal(content(BulkRequest$response)[[1]][17][[1]]$value, "9282")
-
-# 
-# bulk_activities <- list(
-#     "calories",
-#     "caloriesBMR",
-#     "steps",
-#     "distance",
-#     "floors",
-#     "elevation",
-#     "minutesSedentary",
-#     "minutesLightlyActive",
-#     "minutesFairlyActive",
-#     "minutesVeryActive",
-#     "activityCalories"
-# )
-# 
-# lapply(bulk_activities, function(x) {
-#     BulkRequest$get(type = 'day', 
-#                     activity = x, 
-#                     start_date = "2016-01-20", 
-#                     end_date = "2016-02-05", 
-#                     path = "./inst/extdata/tests/")
-#     }
-# )
-
-
+test_that("DataLoader test cases", {
+    # Object creation 
+    expect_output(
+            testObject <- DataLoader$new(), 
+            "Object DataLoader initialized"
+        )
+    
+    # API connection
+    testObject$connect(appname = "cdlr",
+                        key = "227FWR",
+                        secret = "3089e3d1ac5dde1aa00b54a0c8661f42"
+                        )
+    expect_equal(testObject$api_token$app$key, "227FWR")
+    
+    # Test requests 1
+    testObject$request(
+        type = 'day', 
+        activities = c('steps', 'calories', 'minutesLightlyActive'), 
+        start_date = "2016-01-20", 
+        end_date = "2016-02-05", 
+        path = "./inst/extdata/tests/")
+    
+    # Test requests 2
+    testObject$request(
+        type = 'intraday', 
+        activities = c('steps', 'calories', 'distance'), 
+        start_date = "2016-02-01", 
+        path = "./inst/extdata/tests/")
+    }
+)
