3c645fc24810cf779300b353ba220e2d6d26d772
webscale <masterjockey@outlook.com>
2016-03-01 16:20:16 -0600

    still working on this file

--- intra-analyser.R
+++ intra-analyser.R
@@ -1,5 +1,32 @@
-df <- intra
+library(dplyr)
+library(gbm)
 
-a<- cut(intra$timeseq , breaks = 25 , labels = c("night" , "day" ,  "eve" ,"late"))
 
-plot(df$timeseq , log(df$intra.calorie))
+a<- cut(df$timeseq , breaks = c(0,23, 41 , 77 , 90 , 96) , labels = c("night" , "morning" , "day" ,"eve" , "latenight" ) )
+df$slot <- a
+mod<- transform(df,  cumsum.calorie = ave(intra.calorie , date, slot , FUN=cumsum)) 
+mod<- transform(mod, cumsum.steps = ave(intra.steps , date, FUN=cumsum)) 
+mod<- transform(mod, cumsum.level = ave(intra.level , date, FUN=cumsum)) 
+mod<- transform(mod, cumsum.mets = ave(intra.mets , date, FUN=cumsum)) 
+mod<- transform(mod, cumsum.distance = ave(intra.distance , date, FUN=cumsum)) 
+mod<- transform(mod, cumsum.floors = ave(intra.floors , date, FUN=cumsum)) 
+mod<- transform(mod, cumsum.elevation = ave(intra.elevation , date, FUN=cumsum)) 
+
+anaFrame <- mod
+
+anaFrame$date = NULL
+anaFrame$time = NULL
+sl <- anaFrame$slot
+anaFrame$slot <- NULL
+anaMatrix <- data.matrix(anaFrame , rownames.force = NA)
+anaFrame <- as.data.frame(anaMatrix)
+anaFrame$slot <- sl
+
+fit <- gbm(formula = calories~. , data = anaFrame , distribution = "gaussian" , n.trees = 500 ,
+           shrinkage = .05 , interaction.depth = 5 , bag.fraction = .5 , train.fraction = .8 ,
+           cv.folds = 3 , verbose = TRUE)
+
+summary(fit)
+
+str(df)
+
