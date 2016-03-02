f83e298e3a02fee2c9104626bbb6c87778f02441
chrlie <lassence@gmail.com>
2016-03-02 10:41:23 +0100

    Graph: silently removes NAs

--- fitcoach/fitcoach/R/FitChartsUtil.R
+++ fitcoach/fitcoach/R/FitChartsUtil.R
@@ -8,14 +8,13 @@
 
 showCharts <- function(data, x_axis = "date", y_axis) {
     
-    # FIX: need to remove warnings for missing values
     data$Average <- movingAvg(data[[y_axis]]) 
     
     graph <- ggplot(data = data, aes(x = data[[x_axis]])) +
         geom_area(aes(y = data[[y_axis]], fill = y_axis, color = y_axis), alpha = 0.5) +
-        geom_line(aes(y = Average, color = "7-day average")) +
+        geom_line(aes(y = Average, color = "7-day average"), na.rm = TRUE) +
         labs(title = "", x = x_axis, y = y_axis) + 
-        scale_colour_manual(values = c("black", "red"), name ="Legend") +
+        scale_colour_manual(values = c("black", "red"), name = "") +
         guides(fill = "none")
 
     plot(graph)
